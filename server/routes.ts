a00f253bd45b8a3d77b61fccb7a827ef1f7ad1c9
umbertocaputo <44259571+umbertocaputo@users.noreply.github.com>
2026-03-12 16:43:39 +0100

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -1,7 +1,7 @@
 import type { Express } from "express";
 import type { Server } from "http";
 import { storage } from "./storage";
-import { api } from "../shared/routes"; //import { api } from "@shared/routes";
+import { api } from "@shared/routes";
 import { z } from "zod";
 import path from "path";
 import fs from "fs";
